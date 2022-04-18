ac1db63a7bec60c3b57bb05ad0a6434233c67ddb
junmosung <victorsung.js@gmail.com>
2022-04-18 23:44:34 +0900

    refactor: 게임 새로 시작로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,19 +14,18 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 public class Application {
 
     public static final String INPUT_MSG = "숫자를 입력해주세요 : ";
-    public static final int INPUT_FIRST = pickNumberInRange(1, 9);
-    public static final int INPUT_SECOND = pickNumberInRange(1, 9);
-    public static final int INPUT_THIRD = pickNumberInRange(1, 9);
+    public static int INPUT_FIRST = pickNumberInRange(1, 9);
+    public static int INPUT_SECOND = pickNumberInRange(1, 9);
+    public static int INPUT_THIRD = pickNumberInRange(1, 9);
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         try {
-            Game game = new Game(Arrays.asList(INPUT_FIRST, INPUT_SECOND, INPUT_THIRD));
-
             String strMsg;
             boolean status = true;
 
             while (status) {
+                Game game = new Game(Arrays.asList(INPUT_FIRST, INPUT_SECOND, INPUT_THIRD));
                 System.out.print(INPUT_MSG);
                 strMsg = scan.nextLine();
                 if (!isNumberic(strMsg)) {
@@ -60,6 +59,11 @@ public class Application {
                 if (result.getResult().equals("3스트라이크")) {
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                }
+                if (strMsg.equals("1")) {
+                    INPUT_FIRST = pickNumberInRange(1, 9);
+                    INPUT_SECOND = pickNumberInRange(1, 9);
+                    INPUT_THIRD = pickNumberInRange(1, 9);
                 }
             }
         } finally {
